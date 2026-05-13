a44422f22800ca18b2af126bfe174f2a1c015ab9
xingdong168 <xingdong168@users.noreply.github.com>
2026-05-13 19:46:00 +0800

    Fix: continue learning defaults to first lesson (001-002)

---
+++ _next/static/AxUyRTRyVqQ0nCSVuUeRs/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/042bfsul_niuw.js"
+  ],
+  "/_error": [
+    "static/chunks/0a.v-0s.mkwug.js"
+  ],
+  "/nce/book1": [
+    "static/chunks/0863ffyn6~cjl.js"
+  ],
+  "/nce/book1/[id]": [
+    "static/chunks/093d2h._xa0k7.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/nce/book1",
+    "/nce/book1/[id]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/093d2h._xa0k7.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/nce/book1/[id]", [
+  "static/chunks/15_u4tvfbx6xh.js",
+  "static/chunks/0q2c-dcwp5nm5.js",
+  "static/chunks/00..uy386lous.js",
+  "static/chunks/07r~zdbgadq6l.js",
+  "static/chunks/turbopack-0pw8dd6vzonec.js"
+])
