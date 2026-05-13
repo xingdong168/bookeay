65f524bec0b6ac5e948a152a797bca2e90bded9d
xingdong168 <xingdong168@users.noreply.github.com>
2026-05-13 19:39:13 +0800

    Fix: continue learning defaults to first lesson (001-002)

---
+++ _next/static/chunks/042bfsul_niuw.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/01ps857qbjtau.js",
+  "static/chunks/0q2c-dcwp5nm5.js",
+  "static/chunks/00..uy386lous.js",
+  "static/chunks/07r~zdbgadq6l.js",
+  "static/chunks/turbopack-0n9tls719qczr.js"
+])
---
+++ _next/static/chunks/0863ffyn6~cjl.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/nce/book1", [
+  "static/chunks/05xc~lhr5r~ye.js",
+  "static/chunks/0q2c-dcwp5nm5.js",
+  "static/chunks/00..uy386lous.js",
+  "static/chunks/07r~zdbgadq6l.js",
+  "static/chunks/turbopack-006z748.7ym7w.js"
+])
