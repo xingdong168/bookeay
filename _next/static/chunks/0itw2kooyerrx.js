f206c05f397981334a436ea6455e9ff97eff9430
xingdong168 <xingdong168@users.noreply.github.com>
2026-05-13 20:01:54 +0800

    Fix: show Chinese as block, not per-sentence

---
+++ _next/static/chunks/0itw2kooyerrx.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/nce/book1/[id]", [
+  "static/chunks/0yeyur6vie93n.js",
+  "static/chunks/16du2wwzhewvv.js",
+  "static/chunks/0q2c-dcwp5nm5.js",
+  "static/chunks/turbopack-0wt_bbrwb2c8v.js"
+])
---
+++ _next/static/dmC3IZWxYYby7iRPQVr4_/_buildManifest.js
@@ -0,0 +1,26 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/03_0bhsw~kbdo.js"
+  ],
+  "/_error": [
+    "static/chunks/02whmx4y-6smg.js"
+  ],
+  "/nce/book1": [
+    "static/chunks/0srbyfnj-h0mf.js"
+  ],
+  "/nce/book1/[id]": [
+    "static/chunks/0itw2kooyerrx.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/nce/book1",
+    "/nce/book1/[id]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
